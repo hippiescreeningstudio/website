8421af2d9a265afe2280a8770d3ecd65585d3bee
Zhuolun Zhou <hippiescreeningstudio@gmail.com>
2025-06-11 13:28:44 +0200

    update 404 style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -34,16 +34,16 @@ export default function NotFound() {
                 <Intro />
                 <div className="min-h-[60vh] flex flex-col items-center justify-center text-center">
                     <div className="mb-4">
-                        <div className="flex items-center justify-center text-2xl md:text-2xl text-black dark:text-white mb-4">
-                            <span>404</span>
-                            <div className="h-6 w-px bg-black dark:bg-white mx-4"></div>
-                            <span>{language === "en" ? "Page Not Found" : "页面未找到"}</span>
+                        <div className="flex items-center justify-center text-black dark:text-white mb-4">
+                            <span className="font-bold text-3xl md:text-3xl">404</span>
+                            <div className="h-10 w-px bg-black dark:bg-white mx-4"></div>
+                            <span className="text-lg md:text-lg">{language === "en" ? "Page Not Found" : "页面未找到"}</span>
                         </div>
                     </div>
 
                     {/* Countdown and redirect message */}
                     <div className="bg-transparent">
-                        <div className="flex items-center justify-center gap-2 text-black dark:text-white text-xl md:text-xl">
+                        <div className="flex items-center justify-center gap-2 text-black dark:text-white text-lg md:text-lg">
                             <span>
                                 {language === "en"
                                     ? "Redirecting to homepage in"
