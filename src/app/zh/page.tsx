e079f06d9b1592e25d19920f6a04c5a1020eb394
Zhuolun Zhou <hippiescreeningstudio@gmail.com>
2025-07-28 01:01:08 +0200

    resolve next build error

--- src/app/zh/page.tsx
+++ src/app/zh/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SharedIndexPage } from "@/app/page";
+import { SharedIndexPage } from "@/app/_components/shared-index-page";
 
 export default function ChineseIndex() {
     return <SharedIndexPage targetLanguage="zh" />;
