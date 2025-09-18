c8dc0656f45148aa581c50843a789e019cb12612
Zhuolun Zhou <hippiescreeningstudio@gmail.com>
2025-09-18 11:01:08 +0200

    minor

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,18 +23,18 @@ export default function About() {
                         About Us
                     </h1>
                     <div className="prose prose-lg dark:prose-invert max-w-none">
-                        <p className="text-base leading-relaxed mb-6 text-justify">
+                        <p className="text-base leading-relaxed mb-6">
                         Hippie Screening Studio e.V. (HSS) is a non-profit organization dedicated to curating and 
                         screening Chinese-language and Asian films in Munich since 2022.
                         </p>
 
-                        <p className="text-base leading-relaxed mb-6 text-justify">
+                        <p className="text-base leading-relaxed mb-6">
                         Originally founded as a film screening subgroup of the community “706 Munich Hippies,” 
                         HSS began organizing screenings in late 2022. It has since evolved into an independent 
                         curatorial and screening organization and was officially registered as a non-profit association in Munich in January 2024.
                         </p>
 
-                        <p className="text-base leading-relaxed mb-6 text-justify">
+                        <p className="text-base leading-relaxed mb-6">
                         Run by a collective of young Chinese students and cinephiles, HSS focuses on arthouse films from Chinese-speaking 
                         regions and across Asia. The association's mission is to enrich Munich’s cinematic landscape with diverse 
                         Asian voices while fostering cultural exchange and dialogue. As of June 2025, HSS has hosted 33 film screenings 
@@ -43,7 +43,7 @@ export default function About() {
                           Through post-screening Q&A sessions, we actively facilitate conversations between Asian filmmakers and Munich audiences.
                         </p>
 
-                        <p className="text-base leading-relaxed mb-6 text-justify">
+                        <p className="text-base leading-relaxed mb-6">
                         In addition, HSS maintains an active online presence through this website,
                          a WeChat group of nearly 500 film enthusiasts, as well as Instagram and Rednote accounts. 
                          On these platforms, HSS regularly publishes film news, essays and reviews by young critics, 
